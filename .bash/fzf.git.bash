ec752c3cec135424f85be96399058042056d0701
Stanley Shyiko <stanley.shyiko@gmail.com>
2014-09-28 17:35:45 +0300

    Updated fn names in .bash/fzf.git.bash

--- .bash/fzf.git.bash
+++ .bash/fzf.git.bash
@@ -1,14 +1,16 @@
-fgcb() {
+git-interactive-change-branch() {
   local branches branch
   branches=$(git branch) &&
     branch=$(echo "$branches" | fzf +s +m) &&
     git checkout $(echo "$branch" | sed "s/.* //")
 }
 
-fgcc() {
+git-interactive-checkout-commit() {
   local commits commit
   commits=$(git log --pretty=oneline --abbrev-commit --reverse) &&
     commit=$(echo "$commits" | fzf +s +m -e) &&
     git checkout $(echo "$commit" | sed "s/ .*//")
 }
 
+alias gicb=git-interactive-change-branch
+alias gicc=git-interactive-checkout-commit

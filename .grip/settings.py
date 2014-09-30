f3ffe9eff8e7795799c518ee5ccacea7345311d8
Stanley Shyiko <stanley.shyiko@gmail.com>
2014-10-01 00:34:40 +0300

    Added joeyespo/grip settings

---
+++ .grip/settings.py
@@ -0,0 +1,2 @@
+# center container (with `.container { width: 790px !important; }`)
+STYLE_URLS = ['https://rawgit.com/shyiko/1e622a5a490794b0457e/raw/4a70496a9e9620b7450f2c264499bd8d3066111b/grip.css']

a8cc5b76f08af772e9a30966b1085d3fdc60ed4b
Stanley Shyiko <stanley.shyiko@gmail.com>
2014-10-01 00:37:13 +0300

    Updated git/hg aliases

--- .bash/git.bash
+++ .bash/git.bash
@@ -6,3 +6,9 @@ function git-ignore-locally() {
   echo "$1" >> .git/info/exclude
 }
 
+alias gs="git st"
+alias ga="git add"
+alias gc="git ci -m"
+alias gp="git p"
+alias gl="git l"
+alias gd="git d"
